fa6c16f0d8b2c294a1528f6ff32a9589119332a4
saisarankanisetty <saisaran7155@gmail.com>
2021-10-05 00:06:15 +0530

    merge

--- sai.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-print subprocess.check_output('git init', shell=True)
-print subprocess.check_output('git add .', shell=True)
-print subprocess.check_output('git commit -m "a commit"', shell=True)
