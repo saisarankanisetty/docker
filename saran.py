1587f2a6e472df6a1b1ba95ff89378d2f7145492
saisarankanisetty <saisaran7155@gmail.com>
2021-10-05 00:19:41 +0530

    "second"

--- saran.py
+++ saran.py
@@ -1,4 +1,4 @@
 import subprocess
 subprocess.call(['git', 'add', '-A'])
-subprocess.call(['git', 'commit', '-m', ])
+subprocess.call(['git', 'commit', '-m','"second"' ])
 subprocess.call(['git','push','--set-upstream','git@github.com:saisarankanisetty/docker.git','master'])
